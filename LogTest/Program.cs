aa2b43535581df5605f01141f508a6bd84e0f734
Agustin978 <loboagus10@gmail.com>
2022-09-09 18:54:05 -0300

    Agregados dos mensajes uno en info y otro en debug que se guardan en la carpata file.txt

--- LogTest/Program.cs
+++ LogTest/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using NLog;
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+//var config = new NLog.Config.LoggingConfiguration();
+NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+
+logger.Info("Goodbye cruel world!!!");
+logger.Debug("Fuck this code doesn't work :(");
